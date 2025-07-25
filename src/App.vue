687e33147745d60794c712f4635b26a4a5625450
Lucas Scholz <lucas.scholz2103@gmail.com>
2025-07-25 11:07:23 +0200

    test for webpage

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <Header></Header>
+  <router-view/>
+</template>
+
+<script>
+
+import Header from '@/components/Header.vue';
+
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+
+    body {
+      font-family: "Roboto", sans-serif;
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
+
+    input {
+      border: 0;
+      border-radius: 1em;
+      width: 100%;
+      height: 2.5em;
+    }
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <RecipeItem/>
+  <RecipeItem/>
+  <RecipeItem/>
+  <RecipeItem/>
+  <RecipeItem/>
+</template>
+
+<script>
+import RecipeItem from '@/components/RecipeItem.vue';
+
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    RecipeItem
+  }
+}
+</script>
